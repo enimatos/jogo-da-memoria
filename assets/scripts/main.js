29cf7dc95681d70dd3d95f6e26dd56ca0aac5ad8
Elaine Matos <enimatos@live.com>
2025-04-21 21:39:52 -0300

    Entrega do desafio

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,55 +1,66 @@
 const emojis = [
-    "🐨",
-    "🐨",
-    "🦩",
-    "🦩",
-    "🦋",
-    "🦋",
-    "🐯",
-    "🐯",
-    "🐮",
-    "🐮",
-    "🐺",
-    "🐺",
-    "😾",
-    "😾",
-    "🐒",
-    "🐒"
+    "https://images.pexels.com/photos/691583/pexels-photo-691583.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/691583/pexels-photo-691583.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/3796766/pexels-photo-3796766.jpeg",
+    "https://images.pexels.com/photos/3796766/pexels-photo-3796766.jpeg",
+    "https://images.pexels.com/photos/96938/pexels-photo-96938.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/96938/pexels-photo-96938.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/887773/pexels-photo-887773.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/887773/pexels-photo-887773.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/1120049/pexels-photo-1120049.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/1120049/pexels-photo-1120049.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/2071873/pexels-photo-2071873.jpeg",
+    "https://images.pexels.com/photos/2071873/pexels-photo-2071873.jpeg",
+    "https://images.pexels.com/photos/137049/pexels-photo-137049.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/137049/pexels-photo-137049.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/1317844/pexels-photo-1317844.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/1317844/pexels-photo-1317844.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
 ];
+
+
 let openCards = [];
 
-let shuffledEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2: -1));
+let shuffledEmojis = emojis.sort(() => (Math.random() > 0.5 ? 1 : -1));
 
-for(let i = 0; i < emojis.length; i++){
-    let box =document.createElement("div");
-    box.className = "item";
-    box.innerHTML = shuffledEmojis[i]
-    box.onclick = handleClick;
-    document.querySelector(".game").appendChild(box);
+for (let i = 0; i < emojis.length; i++) {
+  let box = document.createElement("div");
+  box.className = "item";
+
+  // Adiciona imagem no HTML
+  let img = document.createElement("img");
+  img.src = shuffledEmojis[i];
+  box.appendChild(img);
+
+  box.onclick = handleClick;
+  document.querySelector(".game").appendChild(box);
 }
 
-function handleClick(){
-    if (openCards.length < 2 ){
-        this.classList.add("boxOpen");
-        openCards.push(this);
-    }   
+function handleClick() {
+  if (openCards.length < 2 && !this.classList.contains("boxOpen") && !this.classList.contains("boxMatch")) {
+    this.classList.add("boxOpen");
+    openCards.push(this);
+  }
 
-    if (openCards.length === 2){
-        setTimeout(checkMatch, 500);
-    }
+  if (openCards.length === 2) {
+    setTimeout(checkMatch, 500);
+  }
 }
 
-function checkMatch(){
-    if(openCards[0].innerHTML === openCards[1].innerHTML){
-        openCards[0].classList.add("boxMatch");
-        openCards[1].classList.add("boxMatch");
-    }else{
-        openCards[0].classList.remove("boxOpen");
-        openCards[1].classList.remove("boxOpen"); 
-    }
-    openCards = []
+function checkMatch() {
+  const img1 = openCards[0].querySelector("img").src;
+  const img2 = openCards[1].querySelector("img").src;
 
-    if(document.querySelectorAll(".boxMatch").length === emojis.length){
-        alert("You win!")
-        }
+  if (img1 === img2) {
+    openCards[0].classList.add("boxMatch");
+    openCards[1].classList.add("boxMatch");
+  } else {
+    openCards[0].classList.remove("boxOpen");
+    openCards[1].classList.remove("boxOpen");
+  }
+
+  openCards = [];
+
+  if (document.querySelectorAll(".boxMatch").length === emojis.length) {
+    alert("Você venceu!");
+  }
 }
